2023f07a6f98cebe01dbc7a3f128f9e6aa39fd9b
joao-pedro-safadao <pedrojaosafadao@gmail.com>
2024-10-13 19:05:32 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,157 +1,150 @@
 // menu.js
 document.addEventListener('DOMContentLoaded', function() {
     const menu = `
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Responsivo</title>
     <style>
-    body
-{
-    background-color: white;
-    margin :0
-}
-a{
-    text-decoration:none;
-    color : black;
-    }
-.um
-{
-    background: rgb(120, 49, 153);
-    color: black;
-}
-#dois
-{
-    background: rgb(0, 255, 255);
-    color: black;
-}
-ul
-{
-    list-style: none; 
-    padding: 0 20px;
-    justify-content: space-between;
-    align-items: center;
-}
-.divracismo{
-font-size: 150px;
-text-align:center;
-color: steelblue;
-background-image: linear-gradient(to bottom, rebeccapurple, steelblue, turquoise);
-background-clip: text;
--webkit-background-clip: text;
--webkit-text-fill-color: transparent;
-color: black;
-  
-}
-#pracismo{
-color: black;
-text-align:justify;
-font-size: 25px;
-}
-nav
-{
-    background-color: #0a647a;
-    padding: 0 20px;
-    justify-content: space-between;
-    align-items: center;
-}
-.roboto-condensed {
-  font-family: "Roboto Condensed", sans-serif;
-  font-optical-sizing: auto;
-  font-weight: <weight>;
-  font-style: normal;
-}
-.p3
-{
-    color: black;
-}
-p:hover,
-a:hover {
-color: rgb(218, 218, 0);
-transition : 1s;
-}
-nav, ul 
-{
-    display: flex;
-    flex-direction: row;
-}
-li
-{
-    padding: 0 20px;
-}
-button
-{
-    background-color: transparent;
-    border: none;
-    color: white;
-    font-size: 36px;
-    position: absolute ;
-    right: 10px;
-    top: 10px;
-    display: none;
-}
-}
-li:hover,{
-    cursor:pointer;
-    opacity: 0.5;
-    background-color: 255, 255, 255, 0.2;
-}
-.imagem{
-width:120px;
-}
-.imagem {
-  transition: transform 1.0s ease;
-}
-
-img:hover {
-  transform: rotateY(180deg);
-}
-.imagem:hover {
-width:140px;
-transition:1s;
-}
-
-    align:}
-
-@media (max-width: 768px){
-    nav, ul {
-        flex-direction: column; /*alinha em coluna */
-    }
-    ul{
-        text-align: center;
-        margin: 0;
-        height: 0;
-
-    }
-    nav{
-        padding-top: 0px; /*espaço pra dentro */
-        align-items: flex-start; /*alinha os itens flex no início do container*/
-
-    }
-    h1{
-        margin: 10px;
-
-    }
-    button{
-        display: flex;
-    }
-    li, ul{
-        width: 100%;
-    }
-}
+        body {
+            background-color: white;
+            margin: 0;
+        }
+        a {
+            text-decoration: none;
+            color: black;
+        }
+        .um {
+            background: rgb(120, 49, 153);
+            color: black;
+        }
+        #dois {
+            background: rgb(0, 255, 255);
+            color: black;
+        }
+        ul {
+            list-style: none; 
+            padding: 0;
+            display: flex;
+            justify-content: flex-end; /* Alinha os itens à direita */
+            align-items: center;
+        }
+        .divracismo {
+            font-size: 150px;
+            text-align: center;
+            color: steelblue;
+            background-image: linear-gradient(to bottom, rebeccapurple, steelblue, turquoise);
+            background-clip: text;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+        #pracismo {
+            color: black;
+            text-align: justify;
+            font-size: 25px;
+            padding: 0px 5px;
+            margin: 5px 100px;
+        }
+        nav {
+            background-color: #0a647a;
+            padding: 0 20px;
+            justify-content: space-between;
+            align-items: center;
+            display: flex;
+        }
+        .roboto-condensed {
+            font-family: "Roboto Condensed", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: normal;
+            font-style: normal;
+        }
+        .p3 {
+            color: black;
+        }
+        p:hover, a:hover {
+            color: rgb(218, 218, 0);
+            transition: 1s;
+        }
+        li {
+            padding: 0 20px;
+        }
+        img:hover {
+            transform: rotateY(180deg);
+        }
+        .imagem {
+            width: 120px;
+        }
+        .imagem:hover {
+            width: 140px;
+            transition: 1s;
+        }
+        #menu-toggle-label {
+            display: none;
+            background: none;
+            border: none;
+            color: white;
+            font-size: 30px;
+            cursor: pointer;
+            padding: 10px;
+        }
+        #menu-toggle {
+            display: none;
+        }
+        @media (max-width: 700px) {
+            nav {
+                flex-direction: column;
+                align-items: flex-start;
+            }
+            #menu-toggle-label {
+                display: block;
+            }
+            ul {
+                display: none;
+                flex-direction: column;
+                width: 100%;
+            }
+            #menu-toggle:checked + #menu-toggle-label::before {
+                content: "✕"; /* Muda o ícone para "X" */
+            }
+            #menu-toggle:not(:checked) + #menu-toggle-label::before {
+                content: "☰"; /* Ícone de menu */
+            }
+            #menu-toggle:checked + #menu-toggle-label + ul {
+                display: flex; /* Mostra o menu quando o checkbox é marcado */
+            }
+            .imagem:hover {
+                width: 120px; /* Remove o efeito hover em dispositivos móveis */
+            }
+            li {
+                padding: 10px 0;
+            }
+        }
     </style>
+</head>
+<body>
     <nav>
-        <div class = "logo">
-            <a href ="index.html"><img src = "racismo.png" title = "Inicio" class="imagem"></a>
+        <div>
+            <a href="index.html"><img src="racismo.png" title="Inicio" class="imagem"></a>
         </div>
+        <input type="checkbox" id="menu-toggle">
+        <label for="menu-toggle" id="menu-toggle-label"></label> <!-- Botão para mobile -->
         <ul>
             <li>
-                <p classs="p3"><a href ="oqracismo.html" class="roboto-condensed">O que é Racismo?</a></p>
+                <p><a href="oqracismo.html" class="roboto-condensed">O que é Racismo?</a></p>
             </li>
             <li>
-                <p><a href ="funciona.html" class="roboto-condensed">Como funciona o Futebol?</a></p>
+                <p><a href="funciona.html" class="roboto-condensed">Como funciona o Futebol?</a></p>
             </li>
             <li>
-                <p><a href ="existe.html" class="roboto-condensed">Existe Racismo no futebol?</a></p>
+                <p><a href="existe.html" class="roboto-condensed">Existe Racismo no futebol?</a></p>
             </li>
         </ul>
     </nav>
+</body>
+</html>
+
     `;
     document.body.insertAdjacentHTML('afterbegin', menu);
 });
