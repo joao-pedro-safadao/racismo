4d4f2758f3e64ec7c1a1ecaec898e5495f7016a2
joao-pedro-safadao <pedrojaosafadao@gmail.com>
2024-12-10 18:54:52 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -180,28 +180,4 @@ img:hover {
             subOptions.style.display = 'none';
         }
     });
-
-    function readAloud() {
-        const content = document.getElementById('container').innerText;
-        const speech = new SpeechSynthesisUtterance(content);
-
-        speech.lang = 'pt-BR'; // Define o idioma para português do Brasil
-
-        // Definir a voz preferida, se disponível
-        const voices = window.speechSynthesis.getVoices();
-        const preferredVoice = voices.find(voice => voice.name === 'Google português do Brasil');
-        if (preferredVoice) {
-            speech.voice = preferredVoice;
-        }
-
-        window.speechSynthesis.speak(speech);
-    }
-
-    // Certifique-se de que as vozes estejam carregadas antes de tentar usá-las
-    if (window.speechSynthesis.onvoiceschanged !== undefined) {
-        window.speechSynthesis.onvoiceschanged = () => {
-            const voices = window.speechSynthesis.getVoices();
-            console.log(voices); // Opcional: listar as vozes disponíveis no console
-        };
-    }
 });
